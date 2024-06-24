013fb6e9af6dfb99f0000a53f3e7193d38ad0640
ifeanyihm <iheme.ifeanyi@yahoo.com>
2024-06-24 17:57:41 +0100

    phoenix global

--- src/Utilities/SearchNotFound.tsx
+++ src/Utilities/SearchNotFound.tsx
@@ -7,7 +7,7 @@ function SearchNotFound() {
       <h1>No Property found</h1>
       <p>
         We couldn't find any properties that match your search criteria in this
-        area. filters. <br /> Please try a different location.
+        area. filters. <br /> Please try a different location....
       </p>
     </div>
   );
