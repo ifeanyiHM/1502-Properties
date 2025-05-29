84b94a3121759e8e7ed02afea2accc844ec363d6
ifeanyihm <iheme.ifeanyi@yahoo.com>
2025-05-29 18:36:36 +0100

    hero section updated

--- src/Components/Header/HeaderTextDescription.tsx
+++ src/Components/Header/HeaderTextDescription.tsx
@@ -1,21 +1,29 @@
 import { Link } from "react-router-dom";
+import useProperty from "../../context/useProperty";
 
 function HeaderTextDescription() {
+  const { setSelectedType } = useProperty();
   return (
     <div className="header-text" aria-label="Header Description">
       <p>DISTRESS SALE OFFER</p>
       <h1>
         A 3 Stories <span> Commercial Property</span> at Apapa
       </h1>
-
       <Link
-        to="https://wa.me/2347086080992"
-        target="_blank"
-        rel="noopener noreferrer"
+        to="service/buy"
+        onClick={() => setSelectedType("cfs")}
         className="buy-now-btn"
       >
         Buy Now
       </Link>
+      {/* <Link
+        to="https://wa.me/2347086080992"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="buy-now-btn-whatsapp"
+      >
+        Whatsapp
+      </Link> */}
     </div>
     // <div className="header-text" aria-label="Header Description">
     //   <p>PHOENIX GLOBAL PROPERTIES</p>
