6bd5d9e9a098f17c967312d0ae2f156cde2d86b8
ifeanyihm <iheme.ifeanyi@yahoo.com>
2025-06-16 20:10:56 +0100

    whatsapp updated

--- src/Utilities/ScrollToTop.tsx
+++ src/Utilities/ScrollToTop.tsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from "react";
-import { FaAnglesUp } from "react-icons/fa6";
+import { useEffect, useState } from "react";
+import { FaAnglesUp, FaWhatsapp } from "react-icons/fa6";
 import { useLocation } from "react-router-dom";
 
 export function ScrollToTop() {
@@ -28,14 +28,43 @@ export function ScrollToTop() {
     });
   };
 
+  const handleWhatsAppClick = () => {
+    const phoneNumber = "2347049943393";
+    const message = "Hello, I’m interested in your services!";
+    const encodedMessage = encodeURIComponent(message);
+    const url = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
+    window.open(url, "_blank");
+  };
+
   return (
-    <button
-      onClick={handleScrollToTop}
-      className="scroll-top"
-      style={{ display: isVisible ? "inline" : "none" }}
-    >
-      <FaAnglesUp />
-    </button>
+    <>
+      <button
+        onClick={handleScrollToTop}
+        className="scroll-top"
+        style={{ display: isVisible ? "inline" : "none" }}
+      >
+        <FaAnglesUp />
+      </button>
+
+      <div className="whatsapp">
+        <span
+          style={{
+            display: isVisible ? "inline" : "none",
+          }}
+        >
+          Need help? Chat with us
+        </span>
+        <button
+          onClick={handleWhatsAppClick}
+          className="whatsapp-button"
+          style={{
+            display: isVisible ? "inline" : "none",
+          }}
+        >
+          <FaWhatsapp />
+        </button>
+      </div>
+    </>
   );
 }
 
